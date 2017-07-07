b08be126c99242ed9e7f077f67efe607aeaabd52
Guilherme Varela <guilhermevarela@hotmail.com>
2017-07-07 19:32:38 -0300

    Added theano2

---
+++ ann2/theano2.py
@@ -0,0 +1,11 @@
+'''
+Created on Jul 07, 2017
+
+@author: Varela
+
+Theano basics.
+For the class Data Science: Practical Deep Learning Concepts in Theano and TensorFlow
+https://deeplearningcourses.com/c/data-science-deep-learning-in-theano-tensorflow
+https://www.udemy.com/data-science-deep-learning-in-theano-tensorflow
+
+'''
