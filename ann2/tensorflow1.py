ed4eb60317b005b6974b8942f2dc9fc559f3e951
Guilherme Varela <guilhermevarela@hotmail.com>
2017-08-04 18:15:51 -0300

    tensorflow1.py added

---
+++ ann2/tensorflow1.py
@@ -0,0 +1,11 @@
+'''
+Created on Ago 04, 2017
+
+@author: Varela
+
+Tensorflow basics
+For the class Data Science: Practical Deep Learning Concepts in Theano and TensorFlow
+course url: https://deeplearningcourses.com/c/data-science-deep-learning-in-theano-tensorflow
+lecture url: https://www.udemy.com/data-science-deep-learning-in-theano-tensorflow/learn/v4/t/lecture/4627284?start=0
+
+'''
