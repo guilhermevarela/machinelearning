6595cbf4a934ee5f554de8647d2f8cc9162d6c19
Guilherme Varela <guilhermevarela@hotmail.com>
2017-08-07 14:28:08 -0300

    Dropout regularization: theano finished

--- ann2/dropout_theano.py
+++ ann2/dropout_theano.py
@@ -80,8 +80,8 @@ class ANN(object):
 		W = np.random.randn(M1,K) / np.sqrt(M1)
 		b = np.zeros(K)
 
-		self.W = theano.shared(W, 'W_logreg')
-		self.b = theano.shared(b, 'b_logreg')
+		self.W = th.shared(W, 'W_logreg')
+		self.b = th.shared(b, 'b_logreg')
 
 		#collect parameters for latter use
 		self.params = [self.W, self.b]
@@ -100,10 +100,10 @@ class ANN(object):
 		grads= T.grad(cost, self.params)
 
 		#for momentum 
-		dparams=[theano.shared(np.zeros_like(p.get_value())) for p in self.params]
+		dparams=[th.shared(np.zeros_like(p.get_value())) for p in self.params]
 
 		#for rmsprop
-		cache = [theano.shared(np.ones_like(p.get_values())) for p in self.params]
+		cache = [th.shared(np.ones_like(p.get_value())) for p in self.params]
 
 		new_cache = [decay*c + (1-decay)*g*g for p, c, g in zip(self.params, cache, grads)]
 		new_dparams = [mu*dp - learning_rate*(g/T.sqrt(new_c + 1e-10)) for p, new_c, dp, g in zip(self.params, new_cache, dparams, grads)]
@@ -112,7 +112,7 @@ class ANN(object):
 		] + [
 			(dp, new_dp) for dp, new_dp in zip(dparams, new_dparams)
 		] + [
-			(p, p + new_dp) for p, new_dp in zip(dparams, new_dparams)
+			(p, p + new_dp) for p, new_dp in zip(self.params, new_dparams)
 		]
 
 		train_op = th.function(
@@ -128,7 +128,53 @@ class ANN(object):
 
 		n_batches= N/batch_size
 		costs = [] 
-		
+		for i in xrange(epochs):
+			for j in xrange(n_batches):
+			
+				Xbatch = X[j*batch_size:((j+1)*batch_size)]
+				Ybatch = Y[j*batch_size:((j+1)*batch_size)]
+				train_op(Xbatch, Ybatch)
+				if j % 20 == 0:
+					
+					c, p = cost_predict_op(Xvalid, Yvalid)
+					costs.append(c)				
+					err = error_rate(Yvalid, p)
+					print "i:%d\tj:%d:\tnb:%d\tcost:%.3f\terror_rate:%.3f" % (i, j, n_batches, c, err)
+				
+		if show_fig:			
+			plt.plot(costs)
+			plt.show()
+
+	def forward_train(self, X):
+		Z = X 
+		for h, p in zip(self.hidden_layers, self.dropout_rates[:-1]):
+			mask = self.rng.binomial(n=1, p=p, size=Z.shape)
+			Z = mask * Z 
+			Z = h.forward(Z)
+
+		mask = self.rng.binomial(n=1, p=self.dropout_rates[-1], size=Z.shape)
+		Z = mask * Z 
+		return T.nnet.softmax(Z.dot(self.W) + self.b)
+
+	def forward_predict(self, X):
+		Z = X
+		for h, p in zip(self.hidden_layers, self.dropout_rates[:-1]):
+			Z = h.forward(p * Z)	
+		return T.nnet.softmax((self.dropout_rates[-1]*Z).dot(self.W) + self.b)
+
+	def predict(self, X):
+		pY = self.forward_predict(X)		
+		return T.argmax(pY, axis=1)
+
+def main():
+	#step 1: get the data and define all usual variables
+	X, Y  = get_normalized_data()
+
+	ann = ANN([500, 300], [0.8, 0.5, 0.5])	
+	ann.fit(X, Y, show_fig=True)
+
+if __name__ == '__main__':
+	main()	
 
 
 
