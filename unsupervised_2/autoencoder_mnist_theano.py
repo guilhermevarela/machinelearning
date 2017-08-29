dcae20d6de7324375d7fd49a5a5fcb51419bbaa7
Guilherme Varela <guilhermevarela@hotmail.com>
2017-08-28 21:06:17 -0300

    unsupervised 2: autoencoder tensorflow

--- unsupervised_2/autoencoder_mnist_theano.py
+++ unsupervised_2/autoencoder_mnist_theano.py
@@ -93,6 +93,95 @@ class AutoEncoder(object):
 		Y = T.nnet.sigmoid(Z.dot(self.W.T) + self.bo)
 		return Y
 
+class DNN(object):
+	def __init__(self, hidden_layer_sizes, UnsupervisedModel=AutoEncoder):
+		self.hidden_layers = [] 
+		count=0
+		for M in hidden_layer_sizes:
+			ae = UnsupervisedModel(M, count)
+			self.hidden_layers.append(ae)
+			count +=1 
+
+	def fit(self, X, Y, Xtest, Ytest, pretrain=True, learning_rate=0.01, mu=.99, reg=.1,epochs=1,batch_sz=100,show_fig=False)
+		pretrain_epochs=1
+		if not pretrain: 
+			pretrain_epochs = 0 
+
+		current_input = X 
+		for ae  in self.hidden_layers: 
+			ae.fit(current_input, epochs=pretrain_epochs) 
+			current_input = ae.hidden_op(current_input)
+
+		N = len(Y)
+		K = len(set(Y))
+		W0 = init_weight(self.hidden_layers[-1].M, K) 
+
+		self.W = theano.shared(W0, 'W_logreg')
+		self.b = theano.shared(np.zeros(K), 'b_logreg')
+		self.params = [self.W, self.b] 
+		for ae in self.hidden_layers: 
+			self.params +=ae.forward_params
+
+		self.dW = theano.shared(np.zeros(W0.shape), 'dW_logreg')
+		self.db = theano.shared(np.zeros(K), 'db_logreg')
+		self.dparams = [self.dW, self.db]
+		for ae in self.hidden_layers:
+			self.dparams += ae.forward_dparams
+
+			X_in = T.matrix('X_in')
+			targets = T.ivector('Targets')
+			pY = self.forward(X_in)
+
+			squared_magnitude = [(p*p) for p in self.params] 
+			reg_cost= -T.sum(squared_magnitude)
+			cost = -T.mean(T.log(pY[T.arange(pY.shape[0]), targets])) + reg_cost 
+			prediction = self.predict(X_in)
+			cost_predict_op = theano.function(
+				inputs=[X_in, targets],
+				outputs=[costs,prediction]
+			) 
+			updates= [
+				(p, p + mu*dp - learning_rate*T.grad(cost,p)) for p, dp in zip(self.params, self.dparams)
+			] + [
+				(dp, mu*dp - learning_rate*T.grad(cost,p)) for p, dp in zip(self.params, self.dparams)
+			]
+
+			train_op = theano.function(
+				inputs=[X_in,targets],
+				updates=updates,
+			) 
+			n_batches = N / batch_sz
+			costs=[]
+			print 'supervised training'
+			for i in xrange(epochs):
+				print "epoch:", i
+				X, Y = shuffle(X, Y) 
+				for j in xrange(n_batches):
+					Xbatch =X[j*batch_sz:(j+1)*batch_sz]
+					Ybatch =Y[j*batch_sz:(j+1)*batch_sz]
+					train_op(Xbatch, Ybatch)
+					the_cost, the_prediction = cost_predict_op(Xtest, Ytest) 
+					error = error_rate(the_prediction, Ytest)
+					print "i:%d\tj:%d\tnb:%d\tcost:%.6f\terror:%.3f\t" % (i,j,n_batches,the_cost, error)
+					costs.append(the_cost)
+
+			if show_fig:
+				plt.plot(costs)
+				plt.show() 
+
+	def predict(self, X):
+		return T.argmax(self.forward(X), axis=1)
+
+	def forward(self, X):
+		current_input = X 
+		for ae in self.hidden_layers: 
+			Z = ae.forward_hidden(current_input) 
+			current_input = Z 
+
+		Y = T.nnet.softmax(T.dot(current_input, self.W) + self.b) 
+		return Y
+					
+
 def test_single_autoencoder():
 	Xtrain, Ytrain, Xtest, Ytest = get_mnist()
 
@@ -118,6 +207,8 @@ def test_single_autoencoder():
 		ans= input('Generate another?')
 		if ans and ans[0] in ('n' or 'N'):
 			done= True
+
+
 
 
 if __name__ == '__main__': 
