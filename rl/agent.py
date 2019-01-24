d426b08bfb5d07d7c5f827a7072f65f4782485cc
Guilherme Varela <guilhermevarela@hotmail.com>
2019-01-24 21:51:05 -0200

    reinforcement-learning: tic-tac-toe additions

---
+++ rl/agent.py
@@ -0,0 +1,8 @@
+class Player:
+	def take_action(env):
+		pass
+	def update(env):
+		pass
+	def update_state_history(state):
+		pass
+	
---
+++ rl/environment.py
@@ -0,0 +1,7 @@
+class Environment:
+	def draw_board():
+		pass
+	def game_over():
+		pass
+	def get_state():
+		pass
